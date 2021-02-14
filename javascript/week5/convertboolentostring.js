cca78b2e69fc227e2c544cc881cf7b1277c3dcc3
Jerry Lafume <77629343+JerryLafumeDev@users.noreply.github.com>
2021-02-14 00:24:55 -0500

    Create convertboolentostring.js

---
+++ javascript/week5/convertboolentostring.js
@@ -0,0 +1,9 @@
+// Implement a function which 
+// convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    let x = bool = b
+    let y = string = String(x) 
+    return y;
+      
+}
