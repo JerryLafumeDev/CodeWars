a63723aa269952998e5a59655156fb13b0014c7f
Jerry Lafume <jerry.lafume456@gmail.com>
2021-04-23 10:30:20 -0400

    Week 14

--- javascript/week5/Lostwithout.js
+++
@@ -1,7 +0,0 @@
-// Given an array of integers, return a new array with each value doubled.
-
-
-function maps(x){
-    const y = x.map(x => x*2)
-    return y
-}
--- javascript/week5/helloworld.js
+++
@@ -1,6 +0,0 @@
-// Make a simple function called greet that returns the most-famous "hello world!".
-
-function greet(arr){
-    arr = ["hello", "world!"]
-    return arr.join(' ')
-  }
